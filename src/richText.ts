2512e163ad128f7b2e16171cb32c3e395b960d1c
ninest <parth.kabra@gmail.com>
2020-10-05 02:21:01 +0800

    Add more fonts

--- src/richText.ts
+++ src/richText.ts
@@ -1,9 +1,25 @@
 import { plain, num } from './constants';
 
 export const richTextEffects = {
+  // sans serif
   'bold sans serif': (text: string) => generateUniqueCode(120276, 120812, text),
-  'italic-bold sans serif': (text: string) => generateUniqueCode(119912, null, text),
-  'bold serif': (text: string) => generateUniqueCode(119808, 120782, text)
+  'italic sans serif': (text: string) => generateUniqueCode(120328, null, text),
+  'italic-bold sans serif': (text: string) =>
+    generateUniqueCode(119912, null, text),
+
+  // serif
+  'bold serif': (text: string) => generateUniqueCode(119808, 120782, text),
+  'italic-bold serif': (text: string) => generateUniqueCode(119912, null, text),
+  medieval: (text: string) => generateUniqueCode(120172, null, text),
+
+  // monospace
+  'typewriter':(text: string) => generateUniqueCode(120432, 120822, text),
+
+  // fun
+  circles: (text: string) => generateUniqueCode(9398, 9450, text),
+  'dark circles': (text: string) => generateUniqueCode(127312, null, text, true),
+  squares: (text: string) => generateUniqueCode(127280, null, text, true),
+  'dark squares': (text: string) => generateUniqueCode(127344, null, text, true)
 };
 
 /*
@@ -16,9 +32,11 @@ https://www.rapidtables.com/convert/number/hex-to-decimal.html
 const generateUniqueCode = (
   aValue: number,
   zeroValue: number,
-  text: string
+  text: string,
+  requiresUppercase: boolean = false
 ) => {
   const converted: string[] = [];
+  if (requiresUppercase) text = text.toUpperCase();
   const textArray: string[] = text.split('');
 
   textArray.forEach((char: string) => {
